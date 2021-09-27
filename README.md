0c4511808e10a76ac09046dae3dd5c00de178840
mdofrbn7 <mdofrbn.7@gmail.com>
2021-09-27 23:27:13 +0600

    Created basic structure and writea basic README.md file

---
+++ README.md
@@ -0,0 +1,5 @@
+# DataStructure #
+## Just simple data structure with array include insertion, deletion, search, sort..  ##
+## The test files only executable on linux,and on specific matchine, because of object file generate based on matchine ... So If you want to run it, BUILD before run. ##
+
+
--- SimpleDataStructure.c
+++ SimpleDataStructure.c
@@ -4,64 +4,117 @@ int Insert(int array[]);
 int Delete(int array[]);
 int Search(int array[]);
 int Sort_Bubble(int array[]);
-int AdditionOfTwoMatrix(int matrixOne[], int matrixTwo[]);
-int MultiplicationOfTwoMatrix(int matrixOne[], int matrixTwo[]);
+int AdditionOfTwoMatrix(int matrixOne[3][3], int matrixTwo[3][3]);
+int MultiplicationOfTwoMatrix(int matrixOne[3][3], int matrixTwo[3][3]);
 //I dont understand this one.
 int ManageTwoDataInSameTypeArray();
+
 int main()
 { 
-    int i;
-   //Let an array array[]
-   //get array size
-   int arraySize;
-   scanf("%d", &arraySize);
-   //get array element with for loop
-   int array[arraySize];
-   for(i = 0 ; i> arraySize ; i++ )
-   {
-       scanf("%d", &array[i]);
-   }
-   //Print the array
-   for(i = 0 ; i> arraySize ; i++)
-   {
-       printf("%d", array[i]);
-   }
-   printf("\n");
-   //Condition to choose-> Sort, Search , Delete ,Insert, AdditionOfTwoArray, ManageTwoDataInSameTypeArray;
-   int choose;
-   printf("Choose what do you want : ");
-   scanf("%d",choose );
-   switch(choose)
-   {
-       case 1:
-            //this case call Insert function 
-           break;
-       case 2:
-           //this case call Delete function
+    int n;
+    printf("Choose array or Matrix :\n1.Array\t2.Matrix\n3.ManageTwoDataInSameTypeArray\n");
+    scanf("%d",&n );
+    if(n==1)//it goes to Array related problem
+    {
+     int i;
+     //Let an array array[]
+     //get array size
+     printf("Enter array Size: ");
+     int arraySize;
+     scanf("%d", &arraySize);
+     //get array element with for loop
+     int array[arraySize];
+     printf("Enter array Element:\n");
+     for(i = 0 ; i< arraySize ; i++ )
+     {
+         scanf("%d", &array[i]);
+     }
+     //Print the array
+     for(i = 0 ; i< arraySize ; i++)
+     {
+         printf("%d, ", array[i]);
+     }
+     printf("\n");
+     //Condition to choose-> Sort, Search , Delete ,Insert, AdditionOfTwoMatrix, MultiplicationOfTwoMatrix, ManageTwoDataInSameTypeArray;
+     int choose;
+     printf("Choose what do you want:\n1.Insert an element\n2.Delete an element\n3.Applying Bubble sort\n4.Search an element\n");
+     scanf("%d", &choose );
+     switch(choose)
+        {
+            case 1:
+                //this case call Insert function 
+                Insert(array);
+                break;
+            case 2:
+                //this case call Delete function
+                Delete(array);
+                break;
+            case 3:
+                //this case call Sort_Bubble function
+                Sort_Bubble(array);
+                break;
+            case 4:
+                //this case call Search function
+                Search(array);
+                break;
+            default :
+                printf("Enter only given numbers..\n");
+        }
+    }
+    else if(n == 2 )
+    {
+        printf("Inside Matrix operations...\n");
+        //get the matrix..
+        //call corrosponding functions..
 
-           break;
-       case 3:
-           //this case call Sort_Bubble function
+                //this case call AdditionOfTwoMatrix
+              //  AdditionOfTwoMatrix(matrixOne, matrixTwo);
+                //this case call MultiplicationOfTwoMatrix
+              //  MultiplicationOfTwoMatrix(matrixOne, matrixTwo);
 
-           break;
-       case 4:
-           //this case call Search function
-
-           break;
-       case 5:
-           //this case call AdditionOfTwoArray
-
-           break;
-       case 6:
-           //this case call ManageTwoDataInSameTypeArray
-
-           break;
-       case 7:
-           //this case call MultiplicationOfTwoMatrix
-           break;
-       default :
-   }
-
+    }
+    else if(n == 3)
+    {
+        printf("Inside Managed arrays....\n");
+                //this case call ManageTwoDataInSameTypeArray 
+    //            ManageTwoDataInSameTypeArray();
+    }
+    else
+        printf("Enter valid number\n");
    return 0;
 
+}
+
+int Insert(int array[])
+{
+printf("Successfull to call function Insert\n");
+}
+int Delete(int array[])
+{
+
+printf("Successfull to call function Delete\n");
+}
+int Search(int array[])
+{
+printf("Successfull to call function Search\n");
+
+}
+int Sort_Bubble(int array[])
+{
+printf("Successfull to call function Sort_Bubble\n");
+
+}
+int AdditionOfTwoMatrix(int matrixOne[3][3], int matrixTwo[3][3])
+{
+printf("Successfull to call function AdditionOfTwoMatrix\n");
+
+}
+int MultiplicationOfTwoMatrix(int matrixOne[3][3], int matrixTwo[3][3])
+{
+printf("Successfull to call function MultiplicationOfTwoMatrix\n");
+
+}
+int ManageTwoDataInSameTypeArray()
+{
+printf("Successfull to call function ManageTwoDataInSameTypeArray\n");
 }
